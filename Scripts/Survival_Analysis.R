7446ce559cbc65063dc18f14cb33043c3fa5174d
Fatemeh Hedayat <fatemeh.hedayat3@gmail.com>
2024-10-29 00:51:52 +0330

    Updated

--- Scripts/Survival_Analysis.R
+++ Scripts/Survival_Analysis.R
@@ -49,5 +49,47 @@ exs$os.event <- cli[rownames(exs), "os.event"]
 
 # Remove samples with missing clinical data
 exs <- na.omit(exs)
-write.table(exs, file = "D:\Project\prostate\Cox_regression/Z_score.txt",
+write.table(exs, file = "Cox_regression/Z_score.txt",
             quote = FALSE, sep = "\t")
+
+#--------------------- Calculate Hazard Ratios for All DEGs ---------------------
+# Create Cox regression formulas for each gene
+univ_formulas <- sapply(covariates, function(x) {
+  as.formula(paste('Surv(time = os.days, event = os.event) ~', x))
+})
+
+
+# Fit Cox models for each gene
+univ_models <- lapply(univ_formulas, function(x) {
+  coxph(x, data = exs)
+})
+
+# Extract results from each model
+univ_results <- lapply(univ_models, function(x) {
+  x <- summary(x)
+  p.value <- signif(x$wald["pvalue"], 4)
+  wald.test <- signif(x$wald["test"], 4)
+  beta <- signif(x$coef[1], 4)              # Coefficient beta
+  HR <- signif(x$coef[2], 4)                # Hazard Ratio
+  HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+  HR.confint.upper <- signif(x$conf.int[,"upper .95"], 2)
+  HR <- paste0(HR, " (", HR.confint.lower, "-", HR.confint.upper, ")")
+  c(beta = beta, `HR (95% CI for HR)` = HR, wald.test = wald.test, p.value = p.value)
+            names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                       })
+# Convert results to data frame and save
+res <- t(as.data.frame(univ_results, check.names = FALSE))
+res <- as.data.frame(res)
+res$symbol <- rownames(res)
+write.table(res, "Cox_regression/cox_regression.txt", quote = F, sep = "\t")
+
+res <- subset(res,`HR (95% CI for HR)`>1 & p.value < 0.05)
+write.table(res, "Cox_regression/Filtered_cox_regression.txt", quote = F, sep = "\t")
+
+# After cox regression > bad prognostic genes > PPI > CytoHubba > HUBs 
+
+################################################################################
+#                                    END OF THE SCRIPT                         #
+################################################################################
